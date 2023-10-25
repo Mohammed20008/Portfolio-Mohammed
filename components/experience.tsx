b37d11bbd19b510d7906b2916512176efaf02c8a
Mohammed20008 <mohammedabdalmenem1@gmail.com>
2023-10-26 00:46:10 +0300

    experienceandfooter

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,57 +1,65 @@
 'use client';
-
 import React from 'react';
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import { Chrono } from 'react-chrono';
+import Heading from './Heading';
 import { experiencesData } from '@/lib/data';
 import { useSectionInView } from '@/lib/hooks';
-import Heading from './Heading';
-import { useTheme } from '@/context/theme-context';
+import { motion } from 'framer-motion';
+import { LuGraduationCap } from 'react-icons/lu';
+// import Image from 'next/legacy/image';
+// // import {
+// //   VerticalTimeline,
+// //   VerticalTimelineElement,
+// // } from 'react-vertical-timeline-component';
 
 export default function Experience() {
-  const { ref } = useSectionInView('Experience');
-  const { theme } = useTheme();
-
+  const { ref } = useSectionInView('Experience', 0.5);
+  const experienceItems = experiencesData.map((item, index) => ({
+    title: item.title,
+    cardTitle: item.title,
+    cardSubtitle: item.location,
+    cardDetailedText: item.description,
+    icon: item.icon,
+  }));
   return (
-    <section id='experience' ref={ref} className='scroll-mt-28 mb-28 sm:mb-40'>
-      <Heading>My experience</Heading>
-      <VerticalTimeline lineColor=''>
-        {experiencesData.map((item, index) => (
-          <React.Fragment key={index}>
-            <VerticalTimelineElement
-              contentStyle={{
-                background: 'white',
-
-                boxShadow: 'none',
-                border: '1px solid rgba(0, 0, 0, 0.05)',
-                textAlign: 'left',
-                padding: '1.3rem 2rem',
-              }}
-              contentArrowStyle={{
-                borderRight:
-                  theme === 'light'
-                    ? '0.4rem solid #9ca3af'
-                    : '0.4rem solid rgba(255, 255, 255, 0.5)',
-              }}
-              date={item.date}
-              icon={item.icon}
-              iconStyle={{
-                background: 'white',
-                fontSize: '1.5rem',
-              }}
-            >
-              <h3 className='font-semibold capitalize'>{item.title}</h3>
-              <p className='font-normal !mt-0'>{item.location}</p>
-              <p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>
-                {item.description}
-              </p>
-            </VerticalTimelineElement>
-          </React.Fragment>
-        ))}
-      </VerticalTimeline>
-    </section>
+    <motion.section
+      ref={ref}
+      id='experience'
+      className='scroll-mt-28'
+      initial={{
+        opacity: 0,
+        y: 100,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+      }}
+    >
+      <Heading>My Experience</Heading>
+      <div className='dark:hidden'>
+        <Chrono
+          items={experienceItems}
+          mode='VERTICAL_ALTERNATING'
+          theme={{
+            primary: 'black',
+            cardBgColor: '#f3f4f6',
+            titleColor: 'black',
+            titleColorActive: 'green',
+          }}
+        />
+      </div>
+      <div className='hidden dark:block'>
+        <Chrono
+          items={experienceItems}
+          mode='VERTICAL_ALTERNATING'
+          theme={{
+            primary: 'gray',
+            cardBgColor: '#172554',
+            titleColor: 'white',
+            titleColorActive: 'white',
+          }}
+        ></Chrono>
+      </div>
+    </motion.section>
   );
 }
